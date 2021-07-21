1e81d34dff8beb1f2cf5e21ae3c7f1d51ec393d9
Juan Pablo Carvajal <jcarvajalabrahams@gmail.com>
2021-07-21 14:25:32 -0600

    Se agrego la opcion de intepretar algunas opciones
    Interpreta la opcion de camabiar el nickname del usurio si esta
    disponible. Adicionalmente ahora se elimina el cliente de la lista
    de clientes para liberar el nombre.

--- chatroom/server.py
+++ chatroom/server.py
@@ -60,15 +60,55 @@ class ServerTCP():
                     msg = json.loads(message)
                     if msg["payload"] == conf.DISCONNECT:
                         conn_status = False
-                    else:
-                        self.__broadcast(msg,client["id"])
-                        print("[{}]: {}".format(client["id"],msg["payload"]))
+                    if msg["opt"] != None:
+                        response = self.__check_options(msg,client)
+                        if not response: break
+
+                    self.__broadcast(msg,client["id"])
+                    print("[{}]: {}".format(client["id"],msg["payload"]))
                 except ValueError as err:
                     print(f"[FAILED] Likely incorrect Format or Header\n {err}")
                     conn_status = False
 
         print("Closing Client")
         conn.close()
+        self.__clients.remove(client)
+
+
+    def __check_options(self,msg,client):
+
+        print(msg["opt"])
+
+        valid=True
+        if msg["opt"] == "CHNGID":
+            self.__change_id(client,msg),
+        else:
+            valid=False
+        
+        if not valid:
+            print("[INVALID OPT]")
+        
+        return True
+
+    def __change_id(self,client,msg):
+        available = True
+        response = {
+            "payload":None,
+            "dest":client["id"]
+        }
+        for _client in self.__clients:
+            if _client["id"] == msg["payload"]:
+                available  = False
+
+        if  available:
+            client["id"] = msg["payload"]
+            response["dest"] = client["id"]
+            response["payload"]="Nuevo Nombre: {}".format(msg["payload"])
+        else:
+            response["payload"]="Nickname No Disponible"
+           
+
+        self.__broadcast(response,"[SERVER]")
 
     def _new_client(self,name, conn):
         """Crea un nuevo cliente
@@ -92,19 +132,19 @@ class ServerTCP():
         for client in self.__clients:
             print("[CLIENT]{}".format(client["id"]))
 
-    def __broadcast(self,payload,client_id):
-
+    def __broadcast(self,payload,sender_id):
+        
         payload_msg = {
-            "from":client_id,
+            "from":sender_id,
             "payload":payload["payload"]
 
         }
 
-        dest = None
+        dest=None
         for client in self.__clients:
             if client["id"] == payload["dest"]:
                 dest = client["conn"]
-        
+    
         payload_json = json.dumps(payload_msg).encode(conf.FORMAT)
         payload_header = str(len(payload_json)).encode(conf.FORMAT)
         payload_header += b' ' * (conf.HEADER-len(payload_header))
@@ -112,6 +152,8 @@ class ServerTCP():
         if dest:
             dest.send(payload_header)
             dest.send(payload_json)
+        elif (payload["dest"] == conf.SERVER_ID):
+            pass
         else:
             print("No one to send to")
 
