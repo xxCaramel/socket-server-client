66438a536548fd144d5acdc14984f37adcf62778
Juan Pablo Carvajal <jcarvajalabrahams@gmail.com>
2021-07-21 00:21:23 -0600

    Se crea clase el servidor tcp
    Clase inicial para el servidor IP/TCP que acepta conexiones IPV4.
    Configuración adicional del algunos parametros del servidor se
    encuentran en la clase ServerConf. Crea un thread por cliente para
    aceptar multiples conexiones.

---
+++ chatroom/server.py
@@ -0,0 +1,107 @@
+import socket
+import threading
+import sys
+
+from socket_common import ServerConf as conf
+
+class ServerTCP():
+    '''
+    Modulo que da metodos para crear un servidor IP/TCP que acepta
+    conexiones IPV4. Opcionalmente se configura mediante socket_common.
+    '''
+
+    def __init__(self,server_port = None, server_ip = None):
+        '''Parametros:
+            server_port (int): Puerto del Servidor (default:80)
+            server_ip   (str): Dirección del servidor (default:127.0.0.1)
+        '''
+
+        self.__clients = []
+
+        if server_port:
+            conf.PORT = server_port
+        if server_ip:
+            conf.SERVER_IP = server_ip
+
+        self.__server_socket = socket.socket(
+            socket.AF_INET,socket.SOCK_STREAM
+        )
+
+        self.__server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
+
+    def __bind_socket(self):
+        """Falla si SERVER_IP o PORT estan en uso"""
+        try:
+            self.__server_socket.bind(
+            (conf.SERVER_IP,conf.PORT)
+        )
+        except OSError as err:
+            print(f"[FAILED] {err}")
+            sys.exit()
+
+    
+    def __handle_client(self,conn,addr):
+
+        new_conn_msg = f"[NEW CLIENT] {addr[0]}{addr[1]}\n"
+        #self.__broadcast(new_conn_msg)
+        print(new_conn_msg)
+
+        conn_status = True
+        while conn_status:
+            message_head = conn.recv(conf.HEADER).decode(conf.FORMAT)
+            if message_head:      
+                message_len = int(message_head)
+                message = conn.recv(message_len).decode(conf.FORMAT)
+
+    
+                print(message is conf.DISCONNECT)
+                print(message)
+                print(conf.DISCONNECT)
+                if message == conf.DISCONNECT:
+                    conn_status = False
+                else:
+                    print(f"[{addr[0]}] {message}")
+
+        print("Closing Client")
+        conn.close()
+
+    def _new_client(self,name, conn):
+        """Crea un nuevo cliente
+
+            Parametros:
+                name: identifacion del cliente
+                conn: conexión socket del cliente
+
+            Retorna:
+                Diccionario con keys: id y conn
+        """
+        client = {
+            "id":name,
+            "conn":conn
+        }
+        return client
+    
+    def __display_clients(self):
+        if not self.__clients:
+            return
+        for client in self.__clients:
+            print("[CLIENT]{}".format(client["id"]))
+
+    def __broadcast(self,message):
+        for client in self.__clients:
+            client["conn"].send(message.encode(conf.FORMAT))
+
+    def init(self):
+        '''Inicializa sevidor con los parametros definidos
+           Crea un thread por cliente
+        '''
+        self.__bind_socket()
+        print("[STARTED] {}:{}".format(conf.SERVER_IP,conf.PORT))
+        self.__server_socket.listen(conf.MAX_CONN)
+
+        while True:
+            conn, addr = self.__server_socket.accept()
+            self.__clients.append(self._new_client(addr[0],conn))
+            client_t = threading.Thread(target=self.__handle_client,args=(conn,addr))
+            client_t.start()
